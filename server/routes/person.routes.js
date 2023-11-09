387915468e7075d82ac684eb3d555c107d31477d
jchodon <jigcho27@gmail.com>
2023-11-09 15:43:19 -0500

    controllers and middleware

--- server/routes/person.routes.js
+++ server/routes/person.routes.js
@@ -2,9 +2,6 @@ const express = require("express");
 const router = express.Router();
 const PersonController = require("../controllers/person.controller");
 
-//for authentication
-const { protect } = require("../middleware/authMiddleware.js");
-
 router.get("/", PersonController.index);
 router.post("/register", PersonController.registerUser);
 router.post("/login", PersonController.loginUser);
