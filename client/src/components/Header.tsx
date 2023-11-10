aa108588d1e36f1d0ce0ad6637a16f4d802682d7
Lucie Chevreuil <95937955+sasszz@users.noreply.github.com>
2023-11-10 14:10:35 -0800

    About Us and Landing (#12)
    * Updated Heder
    
    * About Us

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,17 +3,31 @@ import Logo from "../assets/images/logo.png";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-
-
   return (
-    <header className="bg-blue-500 py-4">
+    <header className="bg-[#011638] py-4">
       <div className="container mx-auto flex justify-between items-center">
-        <img src={Logo} alt="Class Connect" className="md:ml-0 h-24" />
-        <Link to="/login">
-          <button className="bg-[#00008B]  text-white rounded-3xl text-xl m-5 p-5 px-10">
-            login
-          </button>
+        <Link to="/">
+          <img src={Logo} alt="Class Connect" className="md:ml-0 h-20" />
         </Link>
+        <div className="items-center flex flex-row gap-10">
+          <ul className="text-white flex flex-row gap-10">
+            <li>
+              <Link to="/about">
+                <button className="hover:text-[#EEC643]">About Us</button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/signup">
+                <button className="hover:text-[#EEC643]">Sign Up</button>
+              </Link>
+            </li>
+          </ul>
+          <Link to="/login">
+            <button className="bg-[#00008B] hover:bg-[#EEC643] hover:text-[#00008B] text-white rounded-3xl border text-xl px-14 py-4">
+              login
+            </button>
+          </Link>
+        </div>
       </div>
     </header>
   );
