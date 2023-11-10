e2e228505d8a3780e814adebea90522ec9dd4f5f
sasszz <luchevreuil@gmail.com>
2023-11-10 11:26:01 -0800

    Minor bug fixes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,24 +1,8 @@
-// eslint-disable-next-line no-unused-vars
-import React from "react";
+import { Link } from "react-router-dom"; // Make sure to import Link if using React Router
 import Header from "../components/Header";
-import "../App.css";
 
 function Home() {
   return (
-    <> 
-      <div className='flex'>
-          <div className='m-5'>
-              <img src={logo} alt='logo' />
-              <Link to="/profile">
-              <button>profile</button>
-              </Link>
-          </div>
-          <div className='m-5'>
-              <Link to='/login'>
-              <button className='bg-[#00008B] text-white rounded-3xl text-xl m-5 p-5 px-10'>login</button>
-              </Link>
-          </div>
-
     <>
       <Header />
       <div className="mt-10">
@@ -26,6 +10,10 @@ function Home() {
         <div className="m-4 font-bold text-3xl text-white italic">
           Where Meeting Meets Fun!
         </div>
+
+        <Link to="/profile">
+          <button>Profile</button>
+        </Link>
       </div>
     </>
   );
